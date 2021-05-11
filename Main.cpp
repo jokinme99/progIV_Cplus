77082f54af7a8a5102885ea562bfa0ded1955753
jokinme99 <jokinme99@opendeusto.es>
2021-05-11 10:18:12 +0200

    primer commit

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 11 may. 2021
+ *      Author: jokin
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}

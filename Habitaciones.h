eba40524b1f9d03fd8f9b20990702be61d8a0ad1
markitos98 <marcos.llorente@opendeusto.es>
2021-06-14 17:15:20 +0200

    sintaxys

--- Habitaciones.h
+++ Habitaciones.h
@@ -9,5 +9,26 @@
 #define HABITACIONES_H_
 
 
+#include "Habitacion.h"
+
+
+class Habitaciones{
+
+private:
+	Habitacion *habitaciones[200];
+	int numHabitaciones;
+public:
+
+	Habitaciones();
+	Habitaciones(const Habitaciones&);
+	~Habitaciones();
+
+	void anyadirHabitacion(Habitacion *h);
+	void rellenarDeUsuarios();
+	void quitarHabitacion();
+	int callbackHabitacion(void *data, int numeroColumnas, char **contadorDeFila, char **nombresColumnas);
+	void imprimirUsuarios();
+	int getNumUsuario();
+};
 
 #endif /* HABITACIONES_H_ */
